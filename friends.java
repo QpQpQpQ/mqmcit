69b66d8cf536e131ca2b191ceb6dd9e3923f3407
EileenShi <32581158+QpQpQpQ@users.noreply.github.com>
2019-06-17 19:04:32 -0400

    Add files via upload

---
+++ friends.java
@@ -0,0 +1,21 @@
+
+public class friends {	
+
+	public static void main(String[] args) {
+
+		String[] friends = { "Emily", "Bob", "Cindy","Christinarule"};
+		for(int i = 0; i < friends.length; i++) {
+			System.out.println(friends[i]);	
+		}
+
+		for(int i = 0; i < friends.length;i++) {
+			if(friends[i].length() > 10){
+				System.out.println(friends[i]);
+			}
+
+		}
+		double d = 5.2;
+		System.out.printf("%8d", d);
+	}
+
+}
