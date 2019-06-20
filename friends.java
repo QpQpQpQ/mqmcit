284c4828cf998ed01ce616a0ba8884c361ca4394
Eileen <mengqing@seas.upenn.edu>
2019-06-20 09:26:45 -0400

    Eileen changes a name

--- friends.java
+++ friends.java
@@ -3,7 +3,7 @@ public class friends {
 
 	public static void main(String[] args) {
 
-		String[] friends = { "Peter", "Apple", "Cindy","Christinarule"};
+		String[] friends = { "Peter", "Banana", "Cindy","Christinarule"};
 		for(int i = 0; i < friends.length; i++) {
 			System.out.println(friends[i]);	
 		}
