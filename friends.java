83bd0f91965ae047fe81880c2615e99bc7ee7b99
Peter <rdfztianpeng@gmail.com>
2019-06-20 09:12:21 -0400

    Peter- Update a name

--- friends.java
+++ friends.java
@@ -3,7 +3,7 @@ public class friends {
 
 	public static void main(String[] args) {
 
-		String[] friends = { "Roger", "Bob", "Cindy","Christinarule"};
+		String[] friends = { "Peter", "Bob", "Cindy","Christinarule"};
 		for(int i = 0; i < friends.length; i++) {
 			System.out.println(friends[i]);	
 		}
