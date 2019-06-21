e38e739fc9aa23c355083f72799ffcf337417d76
Eileen <mengqing@seas.upenn.edu>
2019-06-21 19:16:21 -0400

    The original Name List

--- friends.java
+++ friends.java
@@ -3,7 +3,7 @@ public class friends {
 
 	public static void main(String[] args) {
 
-		String[] friends = { "Peter", "Banana", "Cindy","Christinarule"};
+		String[] friends = { "Peter", "Banana", "Cindy","Christina"};
 		for(int i = 0; i < friends.length; i++) {
 			System.out.println(friends[i]);	
 		}
